480763f1836ef4c31a86e1745bebb0615da80e91
Enrique Urcullo <enriquencido@gmail.com>
2019-09-06 20:54:05 -0400

    commit uno

---
+++ Proyecto11/Proyecto11/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Proyecto11.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Proyecto11/Proyecto11/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Proyecto11.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
